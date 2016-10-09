0dd169c00a83da2d1da7f214703fb7efeeba5aa9
EEtancelin <Etancelin.edouard@gmail.com>
2016-10-09 16:49:48 +0200

    Feature : implement 2 routes interventions and payements

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/interventions'
+
+  get 'pages/payements'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
